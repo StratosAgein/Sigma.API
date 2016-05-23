34ee4092c182f7b46765af2e871c6251f0e6060b
Cami Rodriguez <camiepisode@outlook.com>
2016-05-23 16:17:35 -0500

    Added automatization

--- DevOps/CreateDevVirtualMachine.ps1
+++ DevOps/CreateDevVirtualMachine.ps1
@@ -1,14 +1,21 @@
 # Settings
 $subscription = Import-AzurePublishSettingsFile "PublishFiles\publish.publishsettings"
-$storageAccount = "StratosStorage"
+$storageAccount = "stratosagein"
 $VMFamily = "Ubuntu Server 14.04 LTS"
-$VMName = "<machine name>"
+$VMName = "SABackendNode"
+$VMSize = "Small"
+$DiskSize= 20
+$CloudServiceName = "stratosagein"
 
 # Selecting azure subscription
-Select-AzureSubscription -SubscriptionName $subscription[0].Name –Current
+Select-AzureSubscription -SubscriptionName $subscription[0].Name -Current
 # Selecting storageAccount
 Set-AzureSubscription -SubscriptionName $subscription[0].Name -CurrentStorageAccountName $storageAccount
 
 # Selecting Ubuntu Image
 $UbuntuImage = Get-AzureVMImage | where { $_.ImageFamily -eq $VMFamily } | sort PublishedDate -Descending | select -ExpandProperty ImageName -First 1
 
+$VirtualMachine = New-AzureVMConfig -Name $VMName -InstanceSize $VMSize -ImageName $UbuntuImage
+$VirtualMachine | Add-AzureDataDisk -CreateNew -DiskSizeInGB $DiskSize
+
+New-AzureVM -ServiceName CloudServiceName -VMs $VirtualMachine
