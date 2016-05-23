90ae029ca0187f3a1cdcbb473d6a42d900f1a78b
Cami Rodriguez <camiepisode@outlook.com>
2016-05-23 16:24:34 -0500

    Added more automatization Code

--- DevOps/CreateDevVirtualMachine.ps1
+++ DevOps/CreateDevVirtualMachine.ps1
@@ -5,6 +5,9 @@ $VMFamily = "Ubuntu Server 14.04 LTS"
 $VMName = "SABackendNode"
 $VMSize = "Small"
 $DiskSize= 20
+$DiskLabel = "C"
+$LogicalUnitNumber = 1
+$hcaching = "ReadWrite"
 $CloudServiceName = "stratosagein"
 
 # Selecting azure subscription
@@ -16,6 +19,7 @@ Set-AzureSubscription -SubscriptionName $subscription[0].Name -CurrentStorageAcc
 $UbuntuImage = Get-AzureVMImage | where { $_.ImageFamily -eq $VMFamily } | sort PublishedDate -Descending | select -ExpandProperty ImageName -First 1
 
 $VirtualMachine = New-AzureVMConfig -Name $VMName -InstanceSize $VMSize -ImageName $UbuntuImage
-$VirtualMachine | Add-AzureDataDisk -CreateNew -DiskSizeInGB $DiskSize
+$VirtualMachine | Add-AzureDataDisk -CreateNew -DiskSizeInGB $DiskSize -DiskLabel $DiskLabel -LUN $LogicalUnitNumber -HostCaching $hcaching
+$VirtualMachine | Add-AzureProvisioningConfig -Windows -AdminUsername "thEpisode" -Password "Cami%3_2012"-NoRDPEndpoint -NoWinRMEndpoint
 
 New-AzureVM -ServiceName CloudServiceName -VMs $VirtualMachine
