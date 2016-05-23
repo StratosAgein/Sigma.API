4f080ac307e8df705fede9133bd1fb18a6db411e
Cami Rodriguez <camiepisode@outlook.com>
2016-05-23 15:16:06 -0500

    Added script for create virtual machine

---
+++ DevOps/CreateDevVirtualMachine.ps1
@@ -0,0 +1,14 @@
+# Settings
+$subscription = Import-AzurePublishSettingsFile "C:\SA Git\Sigma.API\DevOps\PublishFiles\publish.publishsettings"
+$storageAccount = "StratosStorage"
+$VMFamily = "Ubuntu Server 14.04 LTS"
+$VMName = "<machine name>"
+
+# Selecting azure subscription
+Select-AzureSubscription -SubscriptionName $subscription[0].Name –Current
+# Selecting storageAccount
+Set-AzureSubscription -SubscriptionName $subscription[0].Name -CurrentStorageAccountName $storageAccount
+
+# Selecting Ubuntu Image
+$UbuntuImage = Get-AzureVMImage | where { $_.ImageFamily -eq $VMFamily } | sort PublishedDate -Descending | select -ExpandProperty ImageName -First 1
+
