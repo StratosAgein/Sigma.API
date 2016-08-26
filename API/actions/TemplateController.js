a237811e9200771f2eafc08d53c835cb70649170
Cami Rodriguez <camiepisode@outlook.com>
2016-08-26 11:10:40 -0500

    Bones for new entities

---
+++ API/actions/TemplateController.js
@@ -0,0 +1,17 @@
+exports.SomeName = {
+  name: 'SomeName',
+  description: 'SomeDescription',
+  outputExample: {
+    
+  },
+  inputs: {
+    
+  },
+  authenticated: true,
+  version: 1.0,
+  run: function(api, data, next){
+      
+      // Do something...
+      next();
+  }
+};
